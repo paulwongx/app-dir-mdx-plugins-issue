df6fc1d6ddfc8aa0bea77da660b7cfd1b6acd9e6
paulwongx <20178251+paulwongx@users.noreply.github.com>
2023-03-12 22:53:31 -0400

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+import { remarkPlugins } from "./lib/mdx/remark.mjs";
+import NextBundleAnalyzer from "@next/bundle-analyzer";
+import nextMDX from "@next/mdx";
+
+const withBundleAnalyzer = NextBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: false,
+});
+
+const withMDX = nextMDX({
+  options: {
+    remarkPlugins,
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["ts", "tsx", "js", "jsx", "mdx"],
+  experimental: {
+    appDir: true,
+    mdxRs: true,
+  },
+}
+
+export default withBundleAnalyzer(withMDX(nextConfig));
+// export default withMDX(nextConfig);
+// export default nextConfig;
+
+// const withMDX = require('@next/mdx')()
+// module.exports = withMDX(nextConfig)
